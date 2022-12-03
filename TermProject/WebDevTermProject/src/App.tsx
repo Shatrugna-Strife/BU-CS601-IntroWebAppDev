c8a8041afe18cfa608ee0b9fa0678d1c34a18951
Shatrugna Korukanti <shatrugnakorukanti@gmail.com>
2022-12-03 16:07:24 -0500

    Project inititalization

---
+++ TermProject/WebDevTermProject/src/App.tsx
@@ -0,0 +1,17 @@
+import { useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import EditableBlock from './components/editableBlock';
+
+function App() {
+  const [count, setCount] = useState("")
+  const shit:any = useRef();
+  return (
+    <>
+      gsdhsdh
+      <EditableBlock/>
+    </>
+  )
+}
+
+export default App
